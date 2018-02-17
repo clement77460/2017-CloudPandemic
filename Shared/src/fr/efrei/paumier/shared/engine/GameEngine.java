a0f110b5e5fd6a320bcc0eb8a8c8f1fab2ba8f66
Edouard Paumier <edouard.paumier@gmail.com>
2018-02-17 18:49:42 +0100

    Adding GameEngine interface and tests for any implementations

---
+++ Shared/src/fr/efrei/paumier/shared/engine/GameEngine.java
@@ -0,0 +1,8 @@
+package fr.efrei.paumier.shared.engine;
+
+import fr.efrei.paumier.shared.events.EventQueue;
+import fr.efrei.paumier.shared.time.TimeManager;
+
+public interface GameEngine extends TimeManager, EventQueue {
+
+}
---
+++ Shared/test/fr/efrei/paumier/shared/engine/BaseGameEngineTests.java
@@ -0,0 +1,148 @@
+package fr.efrei.paumier.shared.engine;
+
+import static org.junit.Assert.*;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.efrei.paumier.shared.engine.GameEngine;
+import fr.efrei.paumier.shared.events.Event;
+import fr.efrei.paumier.shared.events.FakeEvent;
+import fr.efrei.paumier.shared.time.FakeClock;
+import fr.efrei.paumier.shared.time.TimeManager;
+
+public abstract class BaseGameEngineTests {
+
+	private FakeClock clock;
+	private GameEngine manager;
+	
+	@Before
+	public void setUp() {
+		clock = new FakeClock(Clock.fixed(Instant.EPOCH,
+				ZoneId.systemDefault()));
+		manager = createGameEngine(clock);
+	}
+	
+	protected List<Event> eventTriggered = new ArrayList<Event>();
+
+	protected abstract GameEngine createGameEngine(Clock clock);
+
+	@Test
+	public void update_lowerThanAnyTime_nothingHappens() {
+		Event event = createEvent(Duration.ofSeconds(10));
+		manager.register(event);
+
+		clock.advance(Duration.ofSeconds(1));
+		manager.update();
+
+		assertEquals(0, eventTriggered.size());
+	}
+
+	@Test
+	public void update_higherThanOnlyEvent_eventHappens() {
+		Event event = createEvent(Duration.ofSeconds(10));
+		manager.register(event);
+
+		clock.advance(Duration.ofSeconds(15));
+		manager.update();
+
+		assertEquals(1, eventTriggered.size());
+		assertSame(event, eventTriggered.get(0));
+	}
+
+	@Test
+	public void update_multipleSmallTimes_eventHappens() {
+		FakeEvent event = createEvent(Duration.ofSeconds(9), manager);
+		manager.register(event);
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		clock.advance(Duration.ofSeconds(2));
+		manager.update();
+
+		assertEquals(1, eventTriggered.size());
+		assertSame(event, eventTriggered.get(0));
+		assertEquals(Instant.EPOCH.plusSeconds(9), event.getTriggeredInstant());
+	}
+
+	@Test
+	public void update_equalsToOnlyEvent_eventHappens() {
+		Event event = createEvent(Duration.ofSeconds(10));
+		manager.register(event);
+
+		clock.advance(Duration.ofSeconds(10));
+		manager.update();
+
+		assertEquals(1, eventTriggered.size());
+		assertSame(event, eventTriggered.get(0));
+	}
+
+	@Test
+	public void update_betweenTwoEvents_ordered_firstEventHappens() {
+		Event event1 = createEvent(Duration.ofSeconds(10));
+		Event event2 = createEvent(Duration.ofSeconds(20));
+		manager.register(event1, event2);
+
+		clock.advance(Duration.ofSeconds(15));
+		manager.update();
+
+		assertEquals(1, eventTriggered.size());
+		assertSame(event1, eventTriggered.get(0));
+	}
+
+	@Test
+	public void update_betweenTwoEvents_firstEventHappens() {
+		Event event1 = createEvent(Duration.ofSeconds(20));
+		Event event2 = createEvent(Duration.ofSeconds(10));
+		manager.register(event1, event2);
+
+		clock.advance(Duration.ofSeconds(15));
+		manager.update();
+
+		assertEquals(1, eventTriggered.size());
+		assertSame(event2, eventTriggered.get(0));
+	}
+
+	@Test
+	public void update_AfterTwoEvents_firstEventHappensBeforeOther() {
+		Event event1 = createEvent(Duration.ofSeconds(20));
+		Event event2 = createEvent(Duration.ofSeconds(10));
+		manager.register(event1, event2);
+
+		clock.advance(Duration.ofSeconds(30));
+		manager.update();
+
+		assertEquals(2, eventTriggered.size());
+		assertSame(event2, eventTriggered.get(0));
+		assertSame(event1, eventTriggered.get(1));
+	}
+
+	private FakeEvent createEvent(Duration duration) {
+		return new FakeEvent(Instant.EPOCH, duration, null, this.eventTriggered);
+	}
+
+	protected FakeEvent createEvent(Duration duration, TimeManager manager) {
+		return new FakeEvent(Instant.EPOCH, duration, manager,
+				this.eventTriggered);
+	}
+}
