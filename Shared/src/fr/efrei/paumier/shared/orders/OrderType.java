a0ddd5667a36e04db52f331a4cbab3509601b32d
Edouard Paumier <edouard.paumier@gmail.com>
2018-03-17 16:41:14 +0100

    Adding back OrderType

--- Shared/src/fr/efrei/paumier/shared/orders/OrderType.java
+++ Shared/src/fr/efrei/paumier/shared/orders/OrderType.java
@@ -2,5 +2,9 @@ package fr.efrei.paumier.shared.orders;
 
 public enum OrderType {
 	BUILD_SCREENING_CENTER,
+	RESEARCH_IMPROVED_MEDICINE,
+	RESEARCH_IMPROVED_VACCINE,
+	INCREASE_CURFEW,
+	REDUCE_CURFEW,
 	INCREASE_TAXES
 }
